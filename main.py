a33ccadcea8697499f4696d510593d34a69690d3
DetectivEren <55319774+detectiveren@users.noreply.github.com>
2024-04-27 16:52:08 +0100

    update main.py and fix index.html

--- main.py
+++ main.py
@@ -3,18 +3,15 @@
 # Date: 22/02/2024
 
 # Here's where imports are handled
-import requests
-import communicate_with_unsecure_api
-import communicate_with_secure_api
-import decryptData
+import subprocess
 
 projectName = "Cyber API Security Project"
 projectDesc = ("A Cyber-security API Project that will show the difference between \n"
                "an unsecure API and secure API (using encryption) to show how API Security is important \n"
                "in today's world")
 projectCreator = "Eduardo Manuel Costa Moreira"
-projectLatestUpdate = "22/02/2024"
-milestoneNumber = "Milestone 11"
+projectLatestUpdate = "27/04/2024"
+milestoneNumber = "Milestone 14"
 
 # API URLs
 # REMINDER: Re-route all of these when deploying to linux
@@ -41,64 +38,18 @@ def menu():
     welcomeMessage() # Display the welcome message
     print("1. Unsecure API")
     print("2. Secure API")
-    print("3. Decrypt Data")
+    print("3. Start both")
     print("4. Exit")
     while user != 4:
         user = int(input("Enter an API application to use (Type the number): "))
 
         if user == 1:
-            print("NOTE: Ensure unsecure_api.py is running")
-            print("Do you want a bunch of entry data or enter specific data")
-            print("1. Predict Age by name")
-            print("2. Say Hi to your name")
-            print("3. Test Login")
-            print("4. Test Sample Sensitive Data")
-            user = int(input("Enter the type of Insecure API you want to use (Type the number): "))
-            if user == 1:
-                name = input("Enter a name and it will predict your age: ")
-                communicate_with_unsecure_api.API(unsecure_api_local_predict_age + name)
-            if user == 2:
-                user = ""
-                name = input("Enter a name and it will say hi: ")
-                communicate_with_unsecure_api.API(unsecure_api_local_say_hi + name)
-            if user == 3:
-                user = ""
-                email = input("Enter email to login: ")
-                communicate_with_unsecure_api.API(unsecure_api_local_email + email)
-            if user == 4:
-                user = ""
-                number = input("Enter a number between 1 to 15: ")
-                communicate_with_unsecure_api.API(unsecure_api_test_sample_data + number)
+            subprocess.Popen(['python', 'unsecure_api.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
         if user == 2:
-            print("NOTE: Ensure secure_api.py is running")
-            print("Do you want to Predict Age or have the API say Hi (more will be added later)")
-            print("1. Predict Age by name")
-            print("2. Say Hi to your name")
-            print("3. Test Login")
-            print("4. Test Sample Sensitive Data")
-            user = int(input("Enter the type of Secure API you want to use (Type the number): "))
-            if user == 1:
-                name = input("Enter a name and it will predict your age: ")
-                communicate_with_secure_api.API(secure_api_local_predict_age + name)
-            if user == 2:
-                user = ""
-                name = input("Enter a name and it will say hi: ")
-                communicate_with_secure_api.API(secure_api_local_say_hi + name)
-            if user == 3:
-                user = ""
-                email = input("Enter email to login: ") # Email login at the moment, password will be added later
-                communicate_with_secure_api.API(secure_api_local_email + email)
-            if user == 4:
-                user = ""
-                number = input("Enter a number between 1 to 15: ")
-                communicate_with_secure_api.API(secure_api_test_sample_data + number)
-
+            subprocess.Popen(['python', 'secure_api.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
         if user == 3:
-            print("Here you can decrypt the data from the secure API")
-            user_key = input("Enter the key: ")
-            user_token = input("Enter the token: ")
-            local_response = decryptData.decryptData(user_key, user_token)
-            print(local_response)
+            subprocess.Popen(['python', 'unsecure_api.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+            subprocess.Popen(['python', 'secure_api.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 
 
